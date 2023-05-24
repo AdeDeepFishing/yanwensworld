67c093b668c869092c3e1e4ff24c195636410f7e
Yanwen <91364746+AdeDeepFishing@users.noreply.github.com>
2023-05-24 16:22:47 -0700

    Add files via upload
    put on the whole name for art classes.
    add a up arrow to help guide the web

--- scripts.js
+++ scripts.js
@@ -96,6 +96,25 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    //for the back to top button
+    let backToTopButton = document.getElementById('back-to-top-btn');
+
+    // Show the button when the user scrolls down 20px from the top of the document
+    window.onscroll = function() {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 20) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    };
+
+    // When the user clicks on the button, scroll to the top of the document
+    backToTopButton.onclick = function() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    };
+
+
      
 
 });
